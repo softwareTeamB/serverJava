57f171bbb4b122086d4ce9aff8c0a6ddd4173151
michel <micheldaniel2@hotmail.com>
2017-09-21 15:38:10 +0200

    code conventies
    code conventies verbeterd

--- serverJava/src/marktGevens/MainMarktGevens.java
+++ serverJava/src/marktGevens/MainMarktGevens.java
@@ -4,20 +4,18 @@ import mysql.Mysql;
 
 /**
  * Methoden om de markt data op te slaan
+ *
  * @author michel
  */
 public abstract class MainMarktGevens {
-    
+
     Mysql mysql = new Mysql();
-    
+
     public abstract void getMarktData();
-    
-    private void marktDataSave(){
-    
-    }
-    
+
     /**
      * Markt saver
+     *
      * @param high hoogte prijs 24 uur
      * @param low laagste prijs 24 uur
      * @param volume volume in de coin naam
@@ -27,9 +25,9 @@ public abstract class MainMarktGevens {
      * @param last laatst bepaalde prijss
      * @param idHandelsplaats id van de exchange
      * @param idMarktnaam id van de marktnaamDB
-     * @param history  of de data opgeslagen moet worden
-     * @throws java.lang.Exception als er een error optreed 
-     * 
+     * @param history of de data opgeslagen moet worden
+     * @throws java.lang.Exception als er een error optreed
+     *
      */
     public void marktDataUpdate(
             double high,
@@ -42,37 +40,33 @@ public abstract class MainMarktGevens {
             int idHandelsplaats,
             int idMarktnaam,
             boolean history
-    ) throws Exception{
-        
+    ) throws Exception {
+
         //kijk of de codes al in het database staat
         String countSql = "SELECT COUNT(*) AS total FROM marktupdate"
-                +" WHERE idMarktNaam='"+idMarktnaam+"'"
-                +" AND idHandelsplaats='"+idHandelsplaats+"'";
-        System.out.println(countSql);
+                + " WHERE idMarktNaam='" + idMarktnaam + "'"
+                + " AND idHandelsplaats='" + idHandelsplaats + "'";
         int count = mysql.mysqlCount(countSql);
         System.out.println(count);
-        
+
         //als de markt niet bekend is word het toegevoegd
         //of anders worde de markt update
-        if(count == 0){
-            
+        if (count == 0) {
+
             //sql insert stament
-             String sqlString = "INSERT INTO marktupdate(high, low, volume, volumeBTC, bid, ask, last, idMarktNaam, idHandelsplaats) values "
-                + "('" + high + "', '" + low + "', '" + volume + "', '" + volumeBTC + "', '" + bid + "', '" + ask + "', '" + last + "', '" + idMarktnaam + "', '" + +idHandelsplaats + "')";
-             System.out.println(sqlString);
-             //voeg toe in mysql
-             mysql.mysqlExecute(sqlString);
-             
-             System.out.println("Een markt is toegevoegd in marktupdate");
+            String sqlString = "INSERT INTO marktupdate(high, low, volume, volumeBTC, bid, ask, last, idMarktNaam, idHandelsplaats) values "
+                    + "('" + high + "', '" + low + "', '" + volume + "', '" + volumeBTC + "', '" + bid + "', '" + ask + "', '" + last + "', '" + idMarktnaam + "', '" + +idHandelsplaats + "')";
+
+            //voeg toe in mysql
+            mysql.mysqlExecute(sqlString);
+
+            System.out.println("Een markt is toegevoegd in marktupdate");
         } else {
-            
+
             //update stament
             //String updateSql = ;
             System.err.println("De markt data moet geupdate worden maar die code is nog niet gebouwd");
         }
-        
-        
-        
-    
+
     }
 }
