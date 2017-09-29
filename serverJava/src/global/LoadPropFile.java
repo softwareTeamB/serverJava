643016e4be5bfa2c50867cf6d37b814db1a3f184
michel <micheldaniel2@hotmail.com>
2017-09-29 12:34:39 +0200

    methoden om de prop files te kunnen laden

---
+++ serverJava/src/global/LoadPropFile.java
@@ -0,0 +1,49 @@
+package global;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import serverjava.Installer;
+
+/**
+ *
+ * @author michel
+ */
+public class LoadPropFile {
+
+    /**
+     * methoden om de propties te laden
+     *
+     * @param fileName bestand naam en de map
+     * @return de properties info
+     * @throws IOException file exception
+     */
+    public Properties loadPropFile(String fileName) throws IOException {
+        Properties prop = new Properties();
+        InputStream input;
+
+        try {
+            input = new FileInputStream("./config/config.properties");
+        } catch (FileNotFoundException ex) {
+            System.err.println(ex);
+
+            //run de installer
+            Installer installer = new Installer();
+
+            //reload de funtie
+            return loadPropFile(fileName);
+
+        }
+
+        // load a properties file
+        prop.load(input);
+
+        //close input
+        input.close();
+
+        //return properties file
+        return prop;
+    }
+}
